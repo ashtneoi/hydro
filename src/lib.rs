5e4bd3d65f95e55295b08cd1d44fa99e522c1a63
ashtneoi <ashtneoi@gmail.com>
2018-12-30 22:48:11 -0800

    temp 2018-12-30T22:48:11-08:00

--- src/lib.rs
+++ src/lib.rs
@@ -33,15 +33,30 @@ mod platform {
         .intel_syntax
 
         pivot:
+            push rbx
+            push rbp
+            push r12
+            push r13
+            push r14
+            push r15
+            push 0
+            vstmxcsr [rsp]
+            push 0
+            fstcw [rsp]
+
+            mov r12, rsi # new rip
+            mov r13, rdx # new rsp
+
     "#);
 
     extern "sysv64" {
         fn pivot(
-            rip: *const u8,
-            rsp: *mut u8,
-            next_rip: &mut *const u8,
-            next_rsp: &mut *mut u8,
-            next_rbp: &mut *mut u8,
+            arg: *mut u8, // rdi
+            rip: *const u8, // rsi
+            rsp: *mut u8, // rdx
+            next_rip: &mut *const u8, // rcx
+            next_rsp: &mut *mut u8, // r8
+            next_rbp: &mut *mut u8, // r9
         );
     }
 
@@ -54,7 +69,7 @@ mod platform {
 
     impl Task {
         pub fn start<T: Send>(
-            f: extern "sysv64" fn(*const u8, *const u8) -> !,
+            f: extern "sysv64" fn(&mut T) -> !,
             arg: T,
         ) {
             let mut stack = Vec::with_capacity(1<<18);
@@ -62,7 +77,9 @@ mod platform {
             let mut rsp = stack.last_mut().unwrap() as *mut u8;
 
             unsafe { push_raw(&mut rsp, arg); }
-            let arg_ref = unsafe { &mut *rsp };
+            let arg_ref = rsp;
+
+            rsp = align_mut_ptr_down(rsp, 16);
 
             let t = Task {
                 stack: stack,
@@ -75,6 +92,7 @@ mod platform {
                 let mut nt2 = nt.take().unwrap();
                 unsafe {
                     pivot(
+                        arg_ref,
                         t.rip,
                         t.rsp,
                         &mut nt2.rip,
