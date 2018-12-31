97a1735c5246d5d54eb9c64d8a56c744f1bcd5bd
ashtneoi <ashtneoi@gmail.com>
2018-12-30 21:52:05 -0800

    temp 2018-12-30T21:52:05-08:00

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@ pub(crate) fn align_mut_ptr_down<T>(p: *mut T, alignment: usize) -> *mut T {
     ((p as usize) & !((1<<alignment) - 1)) as *mut T
 }
 
+/// p need not be aligned.
 pub(crate) unsafe fn push_raw<T>(p: &mut *mut u8, val: T) {
+    *p = align_mut_ptr_down(*p, size_of::<T>());
     *p = ((*p as usize) - size_of::<T>()) as *mut u8;
     ptr::write(*p as *mut T, val);
 }
@@ -35,10 +37,11 @@ mod platform {
 
     extern "sysv64" {
         fn pivot(
+            rip: *const u8,
             rsp: *mut u8,
-            f: extern "sysv64" fn(
-                &mut u8, // argument
-            ) -> !,
+            next_rip: &mut *const u8,
+            next_rsp: &mut *mut u8,
+            next_rbp: &mut *mut u8,
         );
     }
 
@@ -57,13 +60,30 @@ mod platform {
             let mut stack = Vec::with_capacity(1<<18);
             unsafe { stack.set_len(1<<18); }
             let mut rsp = stack.last_mut().unwrap() as *mut u8;
-            rsp = align_mut_ptr_down(rsp, 64);
+
+            unsafe { push_raw(&mut rsp, arg); }
+            let arg_ref = unsafe { &mut *rsp };
+
             let t = Task {
                 stack: stack,
                 rip: f as *const u8,
                 rsp: rsp as *mut u8,
                 rbp: rsp as *mut u8, // don't care
             };
+
+            next_task.with(|nt| {
+                let mut nt2 = nt.take().unwrap();
+                unsafe {
+                    pivot(
+                        t.rip,
+                        t.rsp,
+                        &mut nt2.rip,
+                        &mut nt2.rsp,
+                        &mut nt2.rbp,
+                    );
+                }
+                nt.set(Some(nt2));
+            });
         }
     }
 }
