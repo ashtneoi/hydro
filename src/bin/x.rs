108cff06a36fe0c942bceee9136871cc914bcea1
ashtneoi <ashtneoi@gmail.com>
2019-01-21 14:36:26 -0800

    It's gross but it works!

--- src/bin/x.rs
+++ src/bin/x.rs
@@ -2,15 +2,23 @@ extern crate hydro;
 
 use hydro::{next, start};
 
-extern "sysv64" fn go(arg: &mut u32) {
+struct X;
+
+impl Drop for X {
+    fn drop(&mut self) {
+        println!("oh noes i got dropped");
+    }
+}
+
+extern "sysv64" fn go(arg: &mut X) {
     for _ in 0..8 {
-        println!("arg = {}", *arg);
+        println!("go!");
         next();
     }
 }
 
 fn main() {
-    start(go, 82);
+    start(go, X);
     let mut a = 12.0;
     if true {
         a += 13.7;
