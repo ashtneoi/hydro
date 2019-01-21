3c8c83ad6c3588b3408e6dba851a712a8e7ad44c
ashtneoi <ashtneoi@gmail.com>
2019-01-20 16:25:15 -0800

    Going to just add that `remove` arg to next()

--- src/lib.rs
+++ src/lib.rs
@@ -35,15 +35,6 @@ mod platform {
     use std::sync::Mutex;
 
     extern "sysv64" {
-        fn start_inner(
-            arg: *mut u8, // rdi
-            rip: *const u8, // rsi
-            rsp: *mut u8, // rdx
-            save_rip: *mut *const u8, // rcx
-            save_rsp: *mut *mut u8, // r8
-            save_rbp: *mut *mut u8, // r9
-        );
-
         fn pivot_inner(
             rip: *const u8, // rdi
             rsp: *mut u8, // rsi
@@ -52,39 +43,18 @@ mod platform {
             save_rsp: *mut *mut u8, // r8
             save_rbp: *mut *mut u8, // r9
         );
+
+        fn remove_inner(
+            rip: *const u8, // rdi
+            rsp: *mut u8, // rsi
+            rbp: *mut u8, // rdx
+        );
     }
 
     global_asm!(r#"
         .intel_syntax
-        .global start_inner
         .global pivot_inner
-
-        start_inner:
-            push rbp
-            push rbx
-            push r12
-            push r13
-            push r14
-            push r15
-            push 0
-            vstmxcsr [rsp]
-            push 0
-            fstcw [rsp]
-
-            mov r11, rsi // new rip
-            mov r12, rdx // new rsp
-
-            lea rax, [rip+start_inner_back]
-            mov [rcx], rax
-            mov [r8], rsp
-            mov [r9], rbp
-
-            mov rsp, r12
-            // same rdi
-            jmp r11
-
-        start_inner_back:
-            ud2
+        .global remove_inner
 
         pivot_inner:
             push rbp
@@ -124,6 +94,11 @@ mod platform {
             pop rbp
 
             ret  # TODO: far?
+
+        remove_inner:
+            mov rsp, rsi // new rsp
+            mov rbp, rdx // new rbp
+            jmp rdi
     "#);
 
     #[derive(Clone, Copy)]
@@ -167,33 +142,8 @@ mod platform {
         ctx: Option<Context>,
     }
 
-/*
- *    impl Pool for PollingRoundRobinPool {
- *        fn remove(&mut self) {
- *            assert!(is_x86_feature_detected!("avx")); // for vstmxcsr
- *            if self.tasks.back().unwrap().stack.len() == 0 {
- *                panic!("can't remove main task");
- *            }
- *            if self.tasks.len() == 1 { // TODO: error, not panic
- *                panic!("can't remove only task");
- *            }
- *            assert_ne!(self.tasks.len(), 0);
- *
- *            let mut active = self.tasks.pop_back().unwrap();
- *
- *            {
- *                let t = self.tasks.pop_front().unwrap();
- *                self.tasks.push_back(t);
- *            }
- *
- *            unsafe {
- *                active.ctx.unwrap().pivot(
- *                    &self.tasks.back().unwrap().ctx.unwrap()
- *                );
- *            }
- *        }
- *    }
- */
+    impl Pool for PollingRoundRobinPool {
+    }
 
     thread_local! {
         // back = active, front = next
@@ -269,7 +219,7 @@ mod platform {
             };
 
             // We stole active_ctx, so it *must not* survive past the end of
-            // next(), and we *must not* modify the tasks vec.
+            // next(), and we *must not* modify the tasks vec until then.
 
             Some((active_ctx, next_ctx))
         });
@@ -278,6 +228,38 @@ mod platform {
             unsafe {
                 active_ctx.pivot(&next_ctx);
             }
+        }
+    }
+
+    fn remove() {
+        assert!(is_x86_feature_detected!("avx")); // for vstmxcsr
+
+        let (active_stack, next_ctx) = tasks.with(|tt| {
+            let mut tt = tt.borrow_mut();
+
+            if tt.back().unwrap().stack.len() == 0 {
+                panic!("can't remove main task");
+            }
+            if tt.len() == 1 { // TODO: error, not panic
+                panic!("can't remove only task");
+            }
+            assert_ne!(tt.len(), 0);
+
+            let mut active_stack = tt.pop_back().unwrap();
+            active_task.ctx = Some(Context
+
+            {
+                let t = tt.pop_front().unwrap();
+                tt.push_back(t);
+            }
+
+            (active_stack, next_ctx)
+        });
+
+        unsafe {
+            active.ctx.unwrap().pivot(
+                &tt.back().unwrap().ctx.unwrap()
+            );
         }
     }
 }
