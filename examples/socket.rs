a864f8c89f6678cd8db68e4df85bed570a60cba3
ashtneoi <ashtneoi@gmail.com>
2019-02-01 20:41:15 -0800

    Demonstrate TcpListener.hydro_incoming()

--- examples/socket.rs
+++ examples/socket.rs
@@ -1,9 +1,25 @@
 extern crate hydro;
 
+use hydro::net::TcpListenerExt;
 use hydro::task::{next, start};
 use std::net;
 
 fn main() {
-    let lst = net::TcpListener::bind("[::]:10000").unwrap();
-    let (s, _) = lst.accept().unwrap();
+    start(move || {
+        println!("10000 started");
+        let lst = net::TcpListener::bind("[::]:10000").unwrap();
+        lst.set_nonblocking(true).unwrap();
+        for _ in lst.hydro_incoming() {
+            println!("10000");
+        }
+    });
+    start(move || {
+        println!("10001 started");
+        let lst = net::TcpListener::bind("[::]:10001").unwrap();
+        lst.set_nonblocking(true).unwrap();
+        for _ in lst.hydro_incoming() {
+            println!("10001");
+        }
+    });
+    loop { next(); }
 }
